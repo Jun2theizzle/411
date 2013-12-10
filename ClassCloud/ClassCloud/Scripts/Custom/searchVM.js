34d848c9633215bac2c75e6c6b92f5c6e206e354
marchel2 <marchel2@illinois.edu>
2013-12-10 17:12:39 -0600

    Lecture List done

--- ClassCloud/ClassCloud/Scripts/Custom/searchVM.js
+++ ClassCloud/ClassCloud/Scripts/Custom/searchVM.js
@@ -1,7 +1,13 @@
 ﻿var searchVM = function () {
     var self = this;
-
-
+    var Lecture = function (Value, Name,Date) {
+        this.id = Value;
+        this.name = Name;
+        this.date = Date;
+       
+    };
+    self.activeClass = ko.observable();
+    self.LectureList = ko.observableArray();
     self.LoadCourseInfo = function (ID) {
         $.ajax({
             type: 'get',
@@ -45,5 +51,30 @@
 
             }
         });
+    }
+
+    self.DisplayLecture = function (ID) {
+        self.LectureList([]);
+        self.activeClass(ID);
+        $.ajax({
+            type: 'get',
+            dataType: 'json',
+            cache: false,
+            url: '/home/loadcourseinfo/' + ID,
+            data: {},
+            success: function (response) {
+                console.log(response);
+                
+                var Lectures = response.Lectures;//grab the lectures
+                for (var i = 0; i < Lectures.length; i++) {
+
+                   // parsedDate = new Date(parseInt(Lectures[i].Date.replace("/Date(", "").replace(")/", ""), 10));
+                    self.LectureList.push(new Lecture(Lectures[i].ID, Lectures[i].Name, Lectures[i].Date));
+                }
+                console.log(self.LectureList());
+
+            }
+        });
+
     }
 }
